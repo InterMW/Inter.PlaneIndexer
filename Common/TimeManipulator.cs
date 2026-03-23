e707361360ee359aa5c344bafb3340e7319b2980
Joseph-Melberg <josephbmelbergthird@outlook.com>
2026-03-23 16:02:13 +0000

    shot in the dark

--- Common/TimeManipulator.cs
+++ Common/TimeManipulator.cs
@@ -4,9 +4,10 @@ public static class TimeManipulator
 {
     public static long ToLastMinInSec(this long time)
     {
-        var dateTime = DateTime.UnixEpoch.AddSeconds(time);        
-        dateTime = dateTime.AddSeconds(0-dateTime.Second);
-        var result = (long)(dateTime - DateTime.UnixEpoch).TotalSeconds;
-        return result;
+        return time - (time % 60);
+        // var dateTime = DateTime.UnixEpoch.AddSeconds(time);        
+        // dateTime = dateTime.AddSeconds(0-dateTime.Second);
+        // var result = (long)(dateTime - DateTime.UnixEpoch).TotalSeconds;
+        // return result;
     } 
 }
